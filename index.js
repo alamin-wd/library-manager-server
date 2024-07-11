1f9320557de4af7f80d77ff4f7179266b5a8a3ef
alamin-wd <mdalamin67198@gmail.com>
2024-07-11 23:34:50 +0600

    Get Single Books from DB

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ObjectId, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,10 +9,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// console.log("user", process.env.DB_USER)
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ygq6chl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
     serverApi: {
         version: ServerApiVersion.v1,
@@ -23,7 +21,6 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
-        // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
 
         const allBooksCollection = client.db('libraryManagerDB').collection('allBooks');
@@ -32,11 +29,21 @@ async function run() {
         app.get('/allBooks', async (req, res) => {
             const cursor = allBooksCollection.find();
             const result = await cursor.toArray();
-
             res.send(result);
-        })
+        });
 
-        // Send a ping to confirm a successful connection
+        // Get a single book by ID
+        app.get('/allBooks/:id', async (req, res) => {
+            const { id } = req.params;
+            const book = await allBooksCollection.findOne({ _id: new ObjectId(id) });
+            if (book) {
+                res.send(book);
+            } else {
+                res.status(404).send({ message: 'Book not found' });
+            }
+        });
+
+
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
@@ -46,7 +53,6 @@ async function run() {
 }
 run().catch(console.dir);
 
-
 app.get('/', (req, res) => {
     res.send('Library Manager server is Running')
 });
@@ -54,3 +60,4 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Library Manager is running on Port: ${port}`)
 });
+
